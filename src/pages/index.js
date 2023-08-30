fe6acf004c5aef01df1c524c81b0930f1df5fd12
Pothana Venkata Mouli Murapaka <64841456+Mouli322@users.noreply.github.com>
2023-08-30 13:39:48 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
               Currently expanding expertise into deep learning, demonstrating a commitment to staying at the forefront of emerging trends. 
               Eager to contribute my versatile skill set to a dynamic data-driven team while continuously enhancing my capabilities through hands-on learning and collaboration.</p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="/P V Mouli Resume.pdf" target={'_blank'}
+                  <Link href="/Pothana Venkata Mouli Resume.pdf" target={'_blank'}
                   className='flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark
